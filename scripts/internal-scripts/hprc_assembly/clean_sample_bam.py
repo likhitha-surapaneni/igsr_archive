62c2213a1ff03370140414938c5c9fb0304a4d59
Likhitha Surapaneni <10923198+likhitha-surapaneni@users.noreply.github.com>
2025-12-02 11:07:06 +0000

    Script cleanup

---
+++ scripts/internal-scripts/hprc_assembly/clean_sample_bam.py
@@ -0,0 +1,22 @@
+import csv
+import os
+import re
+
+filepath = os.path.join("hprc_phase_2_bam.tsv")
+if os.path.isfile(filepath):
+    with open('hprc_bam_clean_sample.tsv','a', newline='', encoding='utf-8') as tsvfile:
+        with open(filepath, newline='') as file:  
+            reader = csv.reader(file, delimiter='\t')
+            for row in reader:
+                fastq_ftp = row[8]
+                sample_name = row[9]
+                match = re.search(r'[a-zA-Z]+[0-9]+[a-zA-Z]', sample_name)
+
+
+                if sample_name == "Human sample from Homo sapiens":
+                    continue
+                elif match:
+                    print(sample_name)
+                    sample_name = sample_name[:-1]
+                line = f"{sample_name}\t{fastq_ftp}\n"
+                tsvfile.write(line) 
